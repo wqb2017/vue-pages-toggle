8228922d8be428a47b1f3ba1c6f782eddd0002d4
wqb2017 <18354270712@163.com>
2018-06-08 19:59:39 +0800

    新增实列

---
+++ example/demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <VuePagesToggle></VuePagesToggle>
+  </div>
+</template>
+
+<script>
+import VuePagesToggle from './components/VuePagesToggle'
+export default {
+  name: 'App',
+  components: {
+    VuePagesToggle
+  }
+}
+</script>
